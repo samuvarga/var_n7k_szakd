08f0ffeeb137a6971a33a2afd259151d247fa421
samuvarga <samuvarga24@gmail.com>
2026-05-23 21:13:06 +0200

    launch file

--- README.md
+++ README.md
@@ -29,5 +29,5 @@ source ~/ros2_ws/install/setup.bash
 </details>
 
 ``` r
-ros2 launch var_n7k_szakd launch_example1.launch.py
+ros2 launch var_n7k_szakd launch_gazebo.launch.py
 ```
---
+++ launch/launch_gazebo.launch.py
@@ -0,0 +1,22 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    # Include the ros_gz_sim launcher to start Gazebo / Ignition
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+    )
+
+    return LaunchDescription([
+        gz_sim,
+    ])
